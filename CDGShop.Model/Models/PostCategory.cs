d7b1720a104b4a574d32650896a868efee8e3965
LYKINGS\Minh Hải <mjnhaj95@gmail.com>
2017-04-25 23:43:12 +0700

    automapper for web

--- CDGShop.Model/Models/PostCategory.cs
+++ CDGShop.Model/Models/PostCategory.cs
@@ -1,11 +1,12 @@
-﻿using System.Collections.Generic;
+﻿using CDGShop.Model.Abstract;
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace CDGShop.Model.Models
 {
     [Table("PostCategories")]
-    public class PostCategory
+    public class PostCategory: Auditable
     {
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
